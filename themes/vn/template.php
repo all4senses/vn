a1c622c114224996e120404a9803f085f9b5542a
all4senses <all4senses@gmail.com>
2013-02-02 19:26:19 +0200

    General Page style

--- themes/vn/template.php
+++ themes/vn/template.php
@@ -732,7 +732,8 @@ function vn_preprocess_search_block_form(&$vars) {
 function vn_form_alter(&$form, &$form_state, $form_id) {
   if ($form_id == 'search_block_form') {
     
-    $form['actions']['submit']['#value'] = '';
+    // Hide a word on the button "Search"
+    ///$form['actions']['submit']['#value'] = '';
     
     // Autohiding hint.
     $default_search_text = t('Search on VoIP Now');

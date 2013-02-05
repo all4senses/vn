c4eb2f120b2f607772570a58fb60dbd96a39fde4
all4senses <all4senses@gmail.com>
2013-02-05 22:38:41 +0200

    Provider page

--- themes/vn/templates/node--provider.tpl.php
+++ themes/vn/templates/node--provider.tpl.php
@@ -216,23 +216,55 @@
         <footer>
           
           
-          <?php 
           
-            
-            
-//            if (isset($content['field_topics'])) {
-//              $tags = NULL;
-//              foreach (element_children($content['field_topics']) as $key) {
-//                $tags .= ($tags ? '<div class="delim">|</div>' : '') . l(t($content['field_topics'][$key]['#title']), 'articles/tags/' . str_replace(' ', '-', drupal_strtolower($content['field_topics'][$key]['#title'])));
-//              }
-//              if ($tags) {
-//                echo '<div class="topics"><div class="title">' . t('TAGS:') . '</div>' . $tags . '</div>';
-//              }
-//            }
-            //print render($content['field_topics']); 
-            //print render($content['links']);
+          
+            <div class="share">
 
-          ?>
+                      <?php $url = 'http://voipnow.org'. url('node/' . $node->nid); 
+                      
+                      /*
+                        
+                      <div class="others">
+                        <!-- ADDTHIS BUTTON BEGIN -->
+                        <script type="text/javascript">
+                        var addthis_config = {
+                            //pubid: "all4senses"
+                        }
+                        var addthis_share =
+                        {
+                          // ... members go here
+                          url: "<?php echo $url?>"
+                        }
+                        </script>
+
+                        <div class="addthis_toolbox addthis_default_style" addthis:url="<?php echo $url?>">
+                          <a href="http://addthis.com/bookmark.php?v=250&amp;pub=all4senses"></a>
+                          <a class="addthis_button_email" title="E-mail this page link"><?php echo t('Email This Post'); ?></a>
+                          <a class="addthis_button_tumblr"></a>
+                          <a class="addthis_button_hackernews"></a>
+                          <a class="addthis_button_digg"></a>
+                          <a class="addthis_button_reddit"></a>
+                          <a class="addthis_button_stumbleupon"></a>
+
+                          <a class="addthis_button_compact"></a>
+                        </div>
+                        <script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pub=all4senses"></script>
+                        <!-- ADDTHIS BUTTON END -->
+
+                      </div>
+                      
+                      */?>
+                      
+
+                      <div class="main">
+
+                        <?php echo vn_blocks_getSocialiteButtons($url, $title); ?>
+                        
+                      </div> <!-- main share buttons -->
+
+                </div>
+
+          
         </footer>
     
       <?php endif; ?>

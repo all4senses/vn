7e796fc7e7299e0e9a37a081a2f6fdb3f5f1dbf2
all4senses <all4senses@gmail.com>
2013-02-07 20:56:54 +0200

    Home page teasers

--- themes/vn/templates/node--article.tpl.php
+++ themes/vn/templates/node--article.tpl.php
@@ -149,9 +149,10 @@
         
          if (!$page) {
             echo '<div class="links">' . l($content['field_categories'][0]['#title'], $content['field_categories'][0]['#href']). '<span class="delim">|</span><span class="submitted">', $created_str, '</span><span class="delim">|</span>' . l('Comments (0)', 'node/' . $node->nid) . '</div>';
+            //echo render($content['comments']);
          }
          else {
-          //echo render($content['comments']);
+          
           //dpm($node);
           //dpm($content);
           

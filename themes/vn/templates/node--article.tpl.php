c55a57ef395a03ac159a6b25ad8282f1fb539de0
all4senses <all4senses@gmail.com>
2013-02-07 14:48:49 +0200

    Home page teasers

--- themes/vn/templates/node--article.tpl.php
+++ themes/vn/templates/node--article.tpl.php
@@ -41,91 +41,52 @@
           </h2>
           <?php endif; ?> 
 
+          <?php 
 
-          <span class="submitted">
-            <?php 
-            
               //$created_str = date('F d, Y \a\t g:ia', $node->created);
               $created_str = date('m.d.Y', $node->created);
               $created_rdf = preg_replace('|(.*)content=\"(.*)\"\s(.*)|', '$2', $date); //date('Y-m-d\TH:i:s', $node->created); 
               
-              $paths_with_latest_article = FALSE;
-              if (!$page) {
-                $paths_with_latest_articles = array('/compare-business-voip-providers', '/compare-residential-voip-providers', '/business-voip-features', '/sip-trunking-providers', '/canada-voip', '/internet-fax-service-providers');
-                if ($_SERVER['REQUEST_URI'] == '/' || in_array(@$_SERVER['REDIRECT_URL'], $paths_with_latest_articles)) {
-                  $paths_with_latest_article = TRUE;
-                }
-              }
               
-              $extra_data['guest_author'] = NULL;
-              if (!empty($node->field_extra_data['und'][0]['value'])) {
-                $extra_data = unserialize($node->field_extra_data['und'][0]['value']);
-                //dpm($extra_data);
-                $extra_data['guest_author'] = $author_name = !empty($extra_data['guest_author']) ? $extra_data['guest_author'] : NULL;
-              }
-              
-              if (!$extra_data['guest_author'] && ($page || $node->type == 'article' || $paths_with_latest_article) ) {
-                $authorExtendedData = vn_misc_loadUserExtendedData($node->uid);
-                $author_name = $authorExtendedData->realname;
-              }
-
               if ($page) {
                 
-                if ($node->uid) {
-                  
-                  global $language;
-                  
-                  if (!$extra_data['guest_author']) {
-                    $author_url = url('user/' . $node->uid);
-                    //$gplus_profile = (isset($author->field_u_gplus_profile['und'][0]['safe_value']) && $author->field_u_gplus_profile['und'][0]['safe_value']) ? ' <a class="gplus" title="Google+ profile of ' . $author_name . '" href="' . $author->field_u_gplus_profile['und'][0]['safe_value'] . '?rel=author">(G+)</a>' : '';
-                    $gplus_profile = ($authorExtendedData->field_u_gplus_profile_value) ? ' <a class="gplus" title="Google+ profile of ' . $author_name . '" href="' . $authorExtendedData->field_u_gplus_profile_value . '?rel=author">(G+)</a>' : '';
-                    $author_title = t('!author\'s profile', array('!author' => $author_name));
-                  }
-                  
-                  $submitted = '<span property="dc:date dc:created" content="' . $created_rdf . '" datatype="xsd:dateTime" rel="sioc:has_creator">' .
-                                  'By ' .
-                                  //'<a href="' . $author_url . '" title="View user profile." class="username" lang="' . $language->language . '" xml:lang="' . $language->language . '" about="' . $author_url . '" typeof="sioc:UserAccount" property="foaf:name">' .
-                          
-                                  (!$extra_data['guest_author'] ? '<a href="' . $author_url . '" title="' . $author_title . '" class="username" lang="' . $language->language . '" xml:lang="' . $language->language . '" about="' . $author_url . '" typeof="sioc:UserAccount" property="foaf:name">' . $author_name . '</a>' . $gplus_profile : '<span class="guest-author">' . $author_name . '</span>') .
-                          
-                                  //($node->type == 'article' ? '' : '<span class="delim">|</span>' . $created_str) .
-                                  ', posted on ' . $created_str .
-                          
-                               '</span>';
-                  
-                 
+                $extra_data['guest_author'] = NULL;
+                if (!empty($node->field_extra_data['und'][0]['value'])) {
+                  $extra_data = unserialize($node->field_extra_data['und'][0]['value']);
+                  //dpm($extra_data);
+                  $extra_data['guest_author'] = $author_name = !empty($extra_data['guest_author']) ? $extra_data['guest_author'] : NULL;
                 }
-                else {
-                  $submitted = '<span property="dc:date dc:created" content="' . $created_rdf . '" datatype="xsd:dateTime" rel="sioc:has_creator">' .
-                                  'By' . ':' .
-                                  '<span class="username">' .
-                                    'Guest' .
-                                  '</span>' .
-                                  ($node->type == 'article' ? '' : '<span class="delim">|</span>' . $created_str) .
-                               '</span>';
-                  
+
+                if (!$extra_data['guest_author']) {
+                  $authorExtendedData = vn_misc_loadUserExtendedData($node->uid);
+                  $author_name = $authorExtendedData->realname;
                 }
-                
-                echo $submitted;
-              }
-              else {
-                  if ($paths_with_latest_article) {
-                    // Home page articles teasers.
-                    $type_cations = array('blog_post' => 'Blog', 'news_post' => 'News', 'article' => 'Article');
-                    echo $type_cations[$node->type] . ' - By <span class="author">' , $author_name, '</span>' /*l($author_name, 'user/' . $node->uid, array('attributes' => array('title' => $author_name . '\'s profile', 'class' => 'username')))*/, ' / ', date('F d, Y', $node->created) /*vn_misc_elapsed_time($node->created)*/;
-                  }
-                  else {
-                    if ($node->type == 'article') {
-                      echo 'By' , ': ' , $author_name;
-                    }
-                    else {
-                      echo $created_str;
-                    }
-                  }
-              }
-              
+
+                global $language;
+
+                if (!$extra_data['guest_author']) {
+                  $author_url = url('user/' . $node->uid);
+                  //$gplus_profile = (isset($author->field_u_gplus_profile['und'][0]['safe_value']) && $author->field_u_gplus_profile['und'][0]['safe_value']) ? ' <a class="gplus" title="Google+ profile of ' . $author_name . '" href="' . $author->field_u_gplus_profile['und'][0]['safe_value'] . '?rel=author">(G+)</a>' : '';
+                  $gplus_profile = ($authorExtendedData->field_u_gplus_profile_value) ? ' <a class="gplus" title="Google+ profile of ' . $author_name . '" href="' . $authorExtendedData->field_u_gplus_profile_value . '?rel=author">(G+)</a>' : '';
+                  $author_title = t('!author\'s profile', array('!author' => $author_name));
+                }
+
+                $submitted = '<span property="dc:date dc:created" content="' . $created_rdf . '" datatype="xsd:dateTime" rel="sioc:has_creator">' .
+                                'By ' .
+                                //'<a href="' . $author_url . '" title="View user profile." class="username" lang="' . $language->language . '" xml:lang="' . $language->language . '" about="' . $author_url . '" typeof="sioc:UserAccount" property="foaf:name">' .
+
+                                (!$extra_data['guest_author'] ? '<a href="' . $author_url . '" title="' . $author_title . '" class="username" lang="' . $language->language . '" xml:lang="' . $language->language . '" about="' . $author_url . '" typeof="sioc:UserAccount" property="foaf:name">' . $author_name . '</a>' . $gplus_profile : '<span class="guest-author">' . $author_name . '</span>') .
+
+                                //($node->type == 'article' ? '' : '<span class="delim">|</span>' . $created_str) .
+                                ', posted on ' . $created_str .
+
+                              '</span>';
+               
+
+                echo '<span class="submitted">', $submitted, '</span>';
+              }              
             ?>
-          </span>
+          
 
 
       <?php if (!$page): ?>
@@ -145,41 +106,20 @@
           //dpm($node);
           
           if (!$page) {
-            // $path_with_latest_article is defined above.
-            if ($paths_with_latest_article) {
-              // Show an other teaser on the home page.
-              $extra_data = unserialize($node->field_extra_data['und'][0]['value']);
-              
-              if(empty($extra_data['teaser'])) {
-                $extra_data = vn_misc_getArticleTeaserData('all', $content['body'][0]['#markup'], $node->nid);
-              }
-              
-              
-//              if (isset($extra_data['teaser_home'])) {
-//                dpm('00000');
-//                echo $extra_data['teaser_home'];
-//              }
-//              else 
-                {
-                //dpm('0000011111');
-                //echo $extra_data['teaser_block'];
-                echo $extra_data['teaser'];
-              }
-            }
-            else {
-              // TODO: Temporary check. Should be removed after all articles resave.
+            
+              hide($content['body']);
+
               if (!empty($node->field_a_teaser['und'][0]['value'])) {
-                //dpm('11111');
+                dpm('field_a_teaser is not empty');
                 echo $node->field_a_teaser['und'][0]['value'];
               }
               else {
-                //dpm('2222');
+                dpm('field_a_teaser IS empty');
                 $teaser_data = vn_misc_getArticleTeaserData('all', $content['body'][0]['#markup'], $node->nid);
                 echo $teaser_data['teaser'];
               }
-            }
             
-            hide($content['body']);
+            
           }
           
           $keyword_metatag_name = ($node->type == 'news_post') ? 'news_keywords' : 'keywords';
@@ -201,76 +141,26 @@
 
 
 
-      <?php if ($page): ?>
+
     
-                  <footer>
+      <footer>
 
-                    <div class="share">
+        <div class="share">
 
-                      <?php $url = 'http://voipnow.org'. url('node/' . $node->nid); ?>
-
-                      <?  /*
-                        
-                      <div class="others">
-                        <!-- ADDTHIS BUTTON BEGIN -->
-                        <script type="text/javascript">
-                        var addthis_config = {
-                            //pubid: "all4senses"
-                        }
-                        var addthis_share =
-                        {
-                          // ... members go here
-                          url: "<?php echo $url?>"
-                        }
-                        </script>
-
-                        <div class="addthis_toolbox addthis_default_style" addthis:url="<?php echo $url?>">
-                          <a href="http://addthis.com/bookmark.php?v=250&amp;pub=all4senses"></a>
-                          <a class="addthis_button_email" title="E-mail this page link"><?php echo t('Email This Post'); ?></a>
-                          <a class="addthis_button_tumblr"></a>
-                          <a class="addthis_button_hackernews"></a>
-                          <a class="addthis_button_digg"></a>
-                          <a class="addthis_button_reddit"></a>
-                          <a class="addthis_button_stumbleupon"></a>
-
-                          <a class="addthis_button_compact"></a>
-                        </div>
-                        <script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pub=all4senses"></script>
-                        <!-- ADDTHIS BUTTON END -->
-
-                      </div>
-                      
-                      */?>
-                      
-
-                      <div class="main">
-
-                      <?php echo vn_blocks_getSocialiteButtons($url, $title); ?>
-                        
-                      </div> <!-- main share buttons -->
-
-                    </div>
+          <?php $url = 'http://voipnow.org'. url('node/' . $node->nid); ?>
 
 
-                    <?php 
-                    /*
-                      $tags = NULL;
-                      if (!$target_tags) {
-                        $target_tags = array();
-                      }
-                      
-                        foreach ($target_tags as $key => $value) {
-                          $tags .= ($tags ? '<div class="delim">|</div>' : '') . l($field_tags_current[$key]['#title'], 'taxonomy/term/' . $value['tid']);
-                        }
-                      if ($tags) {
-                        echo '<div class="topics"><div class="title">' . t('TAGS:') . '</div>' . $tags . '<div class="bottom-clear"></div></div>';
-                      }
-                      */
-                    ?>
-                  </footer>
+          <div class="main">
+
+          <?php echo vn_blocks_getSocialiteButtons($url, $title); ?>
+
+          </div> <!-- main share buttons -->
+
+        </div>
+
+      </footer>
     
     
-      <?php endif; ?>
     
       <div class="bottom-clear"></div>
  

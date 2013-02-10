3ccc68bbec43bccf1d98dce8eeedeb5cab7e3875
all4senses <all4senses@gmail.com>
2013-02-10 21:19:39 +0200

    Send message block

--- modules/_VN/vn_misc/js/vn_SendMsgNnewsletterSubscr_fieldHints.js
+++ modules/_VN/vn_misc/js/vn_SendMsgNnewsletterSubscr_fieldHints.js
@@ -3,14 +3,14 @@
   Drupal.behaviors.vn_SendMsgNnewsletterSubscr_fieldHints = {
     attach: function (context, settings) {
       
-      $('#block-vn_blocks-send_msg_n_subscribe input[id="edit-fname"], #block-vn_blocks-send_msg_n_subscribe input[id="edit-lname"], #block-vn_blocks-send_msg_n_subscribe input[id="edit-email"], #block-vn_blocks-send_msg_n_subscribe input[id="edit-message"]').each(function(){
+      $('#block-vn_blocks-send_msg_n_subscribe input[id="edit-fname"], #block-vn_blocks-send_msg_n_subscribe input[id="edit-lname"], #block-vn_blocks-send_msg_n_subscribe input[id="edit-email"], #block-vn_blocks-send_msg_n_subscribe textarea[id="edit-message"]').each(function(){
         if ($(this).val() == '') {
           $(this).val($(this).attr('title'));
           $(this).addClass('blur');
         }
       });
       
-      $('#block-vn_blocks-send_msg_n_subscribe input[id="edit-fname"], #block-vn_blocks-send_msg_n_subscribe input[id="edit-lname"], #block-vn_blocks-send_msg_n_subscribe input[id="edit-email"], #block-vn_blocks-send_msg_n_subscribe input[id="edit-message"]').focus(function(){
+      $('#block-vn_blocks-send_msg_n_subscribe input[id="edit-fname"], #block-vn_blocks-send_msg_n_subscribe input[id="edit-lname"], #block-vn_blocks-send_msg_n_subscribe input[id="edit-email"], #block-vn_blocks-send_msg_n_subscribe textarea[id="edit-message"]').focus(function(){
         
         if ($(this).val() == $(this).attr('title')) {
           $(this).val('');
@@ -19,7 +19,7 @@
         
       });
       
-      $('#block-vn_blocks-send_msg_n_subscribe input[id="edit-fname"], #block-vn_blocks-send_msg_n_subscribe input[id="edit-lname"], #block-vn_blocks-send_msg_n_subscribe input[id="edit-email"], #block-vn_blocks-send_msg_n_subscribe input[id="edit-message"]').blur(function(){
+      $('#block-vn_blocks-send_msg_n_subscribe input[id="edit-fname"], #block-vn_blocks-send_msg_n_subscribe input[id="edit-lname"], #block-vn_blocks-send_msg_n_subscribe input[id="edit-email"], #block-vn_blocks-send_msg_n_subscribe textarea[id="edit-message"]').blur(function(){
         
         if ($(this).val() == '') {
           $(this).val($(this).attr('title'));

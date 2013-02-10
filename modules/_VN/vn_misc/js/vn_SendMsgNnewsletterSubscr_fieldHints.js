6009d0efc165f69723e3f2a430b32e90fbaec027
all4senses <all4senses@gmail.com>
2013-02-10 23:45:23 +0200

    Send message block

--- modules/_VN/vn_misc/js/vn_SendMsgNnewsletterSubscr_fieldHints.js
+++ modules/_VN/vn_misc/js/vn_SendMsgNnewsletterSubscr_fieldHints.js
@@ -9,11 +9,13 @@
           $(this).val($(this).attr('title'));
           $(this).addClass('blur');
         }
+        else if ($(this).val() == $(this).attr('title')) {
+          $(this).addClass('blur');
+        }
       });
       
       //$('#block-vn_blocks-send_msg_n_subscribe input[id="edit-fname"], #block-vn_blocks-send_msg_n_subscribe input[id="edit-lname"], #block-vn_blocks-send_msg_n_subscribe input[id="edit-email"], #block-vn_blocks-send_msg_n_subscribe textarea[id="edit-message"]').focus(function(){
-      $('#block-vn-blocks-send-msg-n-subscribe input[id="edit-fname"], #block-vn-blocks-send-msg-n-subscribe input[id="edit-lname"], #block-vn-blocks-send-msg-n-subscribe input[id="edit-email"], #block-vn-blocks-send-msg-n-subscribe textarea[id="edit-message"]').focus(function(){
-        
+      $('#block-vn-blocks-send-msg-n-subscribe input[id="edit-fname"], #block-vn-blocks-send-msg-n-subscribe input[id="edit-lname"], #block-vn-blocks-send-msg-n-subscribe input[id="edit-email"], #block-vn-blocks-send-msg-n-subscribe textarea[id="edit-message"]').focus(function(){        
         if ($(this).val() == $(this).attr('title')) {
           $(this).val('');
           $(this).removeClass('blur');

b7ba1237617abbb19d6ae6c3fd5c280746763243
all4senses <all4senses@gmail.com>
2013-02-20 01:38:25 +0200

    stickyfloat social buttons

--- modules/_VN/vn_misc/js/vn_floatingSocials.js
+++ modules/_VN/vn_misc/js/vn_floatingSocials.js
@@ -3,13 +3,14 @@
   Drupal.behaviors.vn_floatingSocials = {
     attach: function (context, settings) {
        
-       $(".share").stickyfloat({duration: 100});
+       //$(".preface .share").stickyfloat({duration: 100});
+       ////$(".test.share").stickyfloat({duration: 100});
        
-//       $(".preface .share").stickyfloat({ 
-//         duration: 100, 
-//         //stickToBottom: true,
-//         startOffset: 300
-//       });
+       $(".test.share").stickyfloat({ 
+         duration: 100, 
+         //stickToBottom: true,
+         startOffset: 300
+       });
 
        
     }

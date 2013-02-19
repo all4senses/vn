beb37ac70e56974bc5bf58f9d0bd74810a707ad9
all4senses <all4senses@gmail.com>
2013-02-20 00:30:40 +0200

    stickyfloat social buttons

---
+++ modules/_VN/vn_misc/js/vn_floatingSocials.js
@@ -0,0 +1,12 @@
+(function ($) {
+
+  Drupal.behaviors.vn_floatingSocials = {
+    attach: function (context, settings) {
+       
+       $(".preface.share").stickyfloat({ duration: 400 });
+
+       
+    }
+  };
+
+}(jQuery));

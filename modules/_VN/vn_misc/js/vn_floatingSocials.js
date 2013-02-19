822f392983dc61a19dd76d3b97a17a5507478372
all4senses <all4senses@gmail.com>
2013-02-20 01:42:33 +0200

    stickyfloat social buttons

--- modules/_VN/vn_misc/js/vn_floatingSocials.js
+++ modules/_VN/vn_misc/js/vn_floatingSocials.js
@@ -7,9 +7,10 @@
        ////$(".test.share").stickyfloat({duration: 100});
        
        $(".test.share").stickyfloat({ 
-         duration: 100, 
+         duration: 200, 
          //stickToBottom: true,
-         startOffset: 300
+         //startOffset: 300,
+         offsetY: 50
        });
 
        
